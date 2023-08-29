f9b8e61bc6d11bfd0d7511bb3892f2086503181e
Danilo Lemes <13068064+Danil0v3s@users.noreply.github.com>
2023-08-29 12:56:31 +0100

    Update App.kt

--- src/main/kotlin/ui/app/App.kt
+++ src/main/kotlin/ui/app/App.kt
@@ -3,8 +3,11 @@ package ui.app
 import androidx.compose.foundation.layout.Box
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import repository.GameDataRepository
+import com.diozero.ws281xj.PixelAnimations
+import com.diozero.ws281xj.rpiws281x.WS281x
 import ui.components.dashboards.MainDashboard
 
 @Composable
@@ -13,10 +16,10 @@ fun App() = MaterialTheme {
 //    val sessionState = GameDataRepository.session.collectAsState(null)
     val telemetryState = GameDataRepository.telemetry.collectAsState(null)
 
-//    LaunchedEffect(Unit) {
-//        val driver = WS281x(18, 125, 18)
-//        PixelAnimations.demo(driver)
-//    }
+    LaunchedEffect(Unit) {
+        val driver = WS281x(18, 125, 18)
+        PixelAnimations.demo(driver)
+    }
 
     if (telemetryState.value == null) {
         Box {}
@@ -24,6 +27,3 @@ fun App() = MaterialTheme {
         MainDashboard(telemetryState.value!!.telemetry)
     }
 }
-
-
-
